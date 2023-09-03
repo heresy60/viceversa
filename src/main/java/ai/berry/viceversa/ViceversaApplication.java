e3ff1dc567f05736ece14821a5cb7421eadef2e8
berry <berry@chaoscube.co.kr>
2023-09-03 14:20:17 +0900

    [ add ] - [ 프로젝트 기초 설정 ]
    [
      프로젝트 기초 설정
    ]

---
+++ src/main/java/ai/berry/viceversa/ViceversaApplication.java
@@ -0,0 +1,13 @@
+package ai.berry.viceversa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViceversaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViceversaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/berry/viceversa/ViceversaApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.berry.viceversa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViceversaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
